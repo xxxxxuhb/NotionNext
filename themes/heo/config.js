a1147e1b9acec5d5b3349f24ed06ddc58bac83e9
xhb <1484389028@qq.com>
2026-05-21 18:23:33 +0800

    chore(theme/heo): update site config values
    update hero section titles and links, replace github profile url, adjust social card link

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -21,9 +21,9 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_4: 'DeepSeek',
+  HEO_HERO_TITLE_5: 'deepseek-v4-flash 太便宜啦',
+  HEO_HERO_TITLE_LINK: 'https://www.deepseek.com/',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -54,9 +54,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/xxxxxuhb',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://github.com/xxxxxuhb',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -123,7 +123,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: '/qa',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
